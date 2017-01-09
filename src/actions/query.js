ed6f10fd2762c899b11f3d0b0e9fd3c2b83038b8
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-09 21:58:52 +0000

    timesheet structure and css. will need componenting out

--- src/actions/query.js
+++ src/actions/query.js
@@ -1,27 +1,36 @@
-const Query = {
+class Query {
+  constructor() {
+    this.monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  }
+  formatDate(date) {
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const fullYear = date.getFullYear();
+    return `${day} ${this.monthNames[monthIndex]} ${fullYear}`;
+  }
   padNumber(number) {
     return number <= 9999 ? ('000'+number).slice(-4) : number;
-  },
-  getTimeBlocks: () => {
+  }
+  getTimeBlocks() {
     let array = [];
     for(let i = 0; i < 24; i++) {
       array.push({ id: this.padNumber(i), time: `${i}`, category: null });
       array.push({ id: this.padNumber(`${i}30`), time: '', category: null });
     }
     return array;
-  },
-  getDays: () => {
+  }
+  getDays() {
     const timeBlocks = this.getTimeBlocks();
     const today = new Date();
     let days = [];
     let day = new Date(2017, 0, 1); // 01/01/2017
-    
+
     while(day < today) {
-      days.push({ date: day.toISOString(), times: timeBlocks });
+      days.push({ id: (day.getTime() * 10000) + 621355968000000000, date: this.formatDate(day), times: timeBlocks });
       day.setDate(day.getDate() + 1);
     }
     return days;
   }
 }
 
-export default Query;
+export default new Query();
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -5,42 +5,46 @@ import './home.css';
 class Home extends Component {
   getTimes(array) {
     return array.map((item) => {
-      return <div key={item.time}>{ item.category }</div>
+      const classColour = item.category ? item.category : '';
+      return <div key={item.id} className={classColour}>{ item.category }</div>
     });
   }
   render() {
-    const days = Query.getsDays();
+    const timeBlocks = Query.getTimeBlocks();
+    const days = Query.getDays();
     return (
       <div className="home">
-        <p>Selvaria home page</p>
+        <header>
+          <h2>Timesheet {new Date().getFullYear()}</h2>
+        </header>
         <div>
           <ol>
-          <li>
-            <div className="date">date</div>
-            <div className="times">
-              {
-                timeBlocks.map((item) => {
-                  return (
-                    <div key={item.time}>
-                      { item.time }
-                    </div>
-                  );
-                })
-              }
-            </div>
-          </li>
-           {
-             days.map((item) => {
-                return (
-                  <li key={item.date}>
-                      <div className="date">{ item.date.toString() }</div>
-                      <div className="times">
-                        { this.getTimes(item.timeBlocks) }
+            <li className="list-headers">
+              <div className="date">Date</div>
+              <div className="times">
+                {
+                  timeBlocks.map((item) => {
+                    return (
+                      <div key={item.id}>
+                        { item.time }
                       </div>
-                  </li>
-                );
-             })
-           }
+                    );
+                  })
+                }
+              </div>
+            </li>
+             {
+               days.map((item) => {
+                  return (
+                    <li key={item.id} className="center-contents">
+                        <div className="date">{ item.date }</div>
+                        <div className="times">
+                          { this.getTimes(item.times) }
+                        </div>
+                    </li>
+                  );
+               })
+             }
           </ol>
         </div>
       </div>
