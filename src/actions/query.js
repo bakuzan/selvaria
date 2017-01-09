034d4fbd84a92392cafbe6fd464d2c74c4523b71
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-09 15:55:30 +0000

    Update query.js
    Added a days generator while testing.
    
    Will be updated to server call later.

--- src/actions/query.js
+++ src/actions/query.js
@@ -9,6 +9,18 @@ const Query = {
       array.push({ id: this.padNumber(`${i}30`), time: '', category: null });
     }
     return array;
+  },
+  getDays: () => {
+    const timeBlocks = this.getTimeBlocks();
+    const today = new Date();
+    let days = [];
+    let day = new Date(2017, 0, 1); // 01/01/2017
+    
+    while(day < today) {
+      days.push({ date: day.toISOString(), times: timeBlocks });
+      day.setDate(day.getDate() + 1);
+    }
+    return days;
   }
 }
 
