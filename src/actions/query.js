e9fb86a9668963a7e763d66152208f97f608a0cb
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-09 12:56:13 +0000

    Update query.js
    currently does not call server as only investigating how to structure data/views

--- src/actions/query.js
+++ src/actions/query.js
@@ -1,55 +1,14 @@
 const Query = {
+  padNumber(number) {
+    return number <= 9999 ? ('000'+number).slice(-4) : number;
+  },
   getTimeBlocks: () => {
-  return [
-      { id: '0000', time: '00', category: null },
-      { id: '0030', time: '', category: null },
-      { time: '0100', category: null },
-      { time: '0130', category: null },
-      { time: '0200', category: null },
-      { time: '0230', category: null },
-      { time: '0300', category: null },
-      { time: '0330', category: null },
-      { time: '0400', category: null },
-      { time: '0430', category: null },
-      { time: '0500', category: null },
-      { time: '0530', category: null },
-      { time: '0600', category: null },
-      { time: '0630', category: null },
-      { time: '0700', category: null },
-      { time: '0730', category: null },
-      { time: '0800', category: null },
-      { time: '0830', category: null },
-      { time: '0900', category: null },
-      { time: '0930', category: null },
-      { time: '1000', category: null },
-      { time: '1030', category: null },
-      { time: '1100', category: null },
-      { time: '1130', category: null },
-      { time: '1200', category: null },
-      { time: '1230', category: null },
-      { time: '1300', category: null },
-      { time: '1330', category: null },
-      { time: '1400', category: null },
-      { time: '1430', category: null },
-      { time: '1500', category: null },
-      { time: '1530', category: null },
-      { time: '1600', category: null },
-      { time: '1630', category: null },
-      { time: '1700', category: null },
-      { time: '1730', category: null },
-      { time: '1800', category: null },
-      { time: '1830', category: null },
-      { time: '1900', category: null },
-      { time: '1930', category: null },
-      { time: '2000', category: null },
-      { time: '2030', category: null },
-      { time: '2100', category: null },
-      { time: '2130', category: null },
-      { time: '2200', category: null },
-      { time: '2230', category: null },
-      { time: '2300', category: null },
-      { time: '2330', category: null }
-    ];
+    let array = [];
+    for(let i = 0; i < 24; i++) {
+      array.push({ id: this.padNumber(i), time: `${i}`, category: null });
+      array.push({ id: this.padNumber(`${i}30`), time: '', category: null });
+    }
+    return array;
   }
 }
 
