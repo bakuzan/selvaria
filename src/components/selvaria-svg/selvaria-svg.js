bbd415095ce60662b1560b81107dabc752070a89
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-09 09:38:57 +0000

    Update selvaria-svg.js
    - adjusted x coordinate values to centre letters.
    - removed unused id's
    - swapped word from 'bles' to 'selvaria'

--- src/components/selvaria-svg/selvaria-svg.js
+++ src/components/selvaria-svg/selvaria-svg.js
@@ -5,32 +5,32 @@ class SelvariaSvg extends Component {
   render() {
     return (
       <svg className="selvaria-svg" viewBox="-20 -60 80 80" preserveAspectRatio="xMaxYMax meet" xmlns="http://www.w3.org/2000/svg">
-        <text className="letter hideshow" id="ess" x="0" y="-0">
+        <text className="letter hideshow" x="-3" y="-0">
           S
         </text>
-        <text className="letter hideshow" id="ee" x="0" y="-0">
+        <text className="letter hideshow" x="-3" y="-0">
           E
         </text>
-        <text className="letter hideshow" id="el" x="10" y="-0">
+        <text className="letter hideshow" x="-2" y="-0">
           L
         </text>
-        <text className="letter hideshow" id="vee" x="9" y="-0">
+        <text className="letter hideshow" x="-4" y="-0">
           V
         </text>
-        <text className="letter hideshow" id="eh" x="9" y="-0">
+        <text className="letter hideshow" x="-4" y="-0">
           A
         </text>
-        <text className="letter hideshow" id="arr" x="9" y="-0">
+        <text className="letter hideshow" x="-6" y="-0">
           R
         </text>
-        <text className="letter hideshow" id="eye" x="18" y="-0">
+        <text className="letter hideshow" x="10" y="-0">
           I
         </text>
-        <text className="letter hideshow" id="eh" x="9" y="-0">
+        <text className="letter hideshow" x="-4" y="-0">
           A
         </text>
-        <text id="word" className="diagonal" x="-0" y="-0">
-          BLES
+        <text className="diagonal" x="-0" y="-0" id="word">
+          SELVARIA
         </text>
       </svg>
     );
