73e4616fe64e6e47c08894e3edee1b5c0887c491
bakuzan <imannoyed4days@hotmail.co.uk>
2017-01-16 10:40:14 +0000

    Create time.js
    Starting point for a time for a day.
    Probably need more things added.

---
+++ server/models/time.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const TimeSchema = new Schema({
+  dateTime: Date,
+  category: {
+    type: String,
+    default: null
+  }
+});
+
+TimeSchema.virtual('time').get(() => {
+  const hh = this.dateTime.getHours();
+  const mh = this.dateTime.getMinutes();
+  return `${('0' + hh).slice(-2)}${mm}`;
+});
+
+module.exports = mongoose.model('Time', TimeSchema);
