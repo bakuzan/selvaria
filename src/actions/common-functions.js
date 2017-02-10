8429916c09bf7cdb4e6b975187142610b572a860
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-10 12:34:34 +0000

    Update common-functions.js
    Add the can get next day method to check if it is possible to add a new day.
    
    note: this relies on the latest date of the current query.

--- src/actions/common-functions.js
+++ src/actions/common-functions.js
@@ -3,11 +3,9 @@ import Constants from '../constants/values';
 class CommonService {
   areDatesEqual(dateOne, dateTwo) {
     const d1 = new Date(dateOne);
-    d1.setHours(0);
-    d1.setMinutes(0);
+    d1.setHours(0,0,0,0);
     const d2 = new Date(dateTwo);
-    d2.setHours(0);
-    d2.setMinutes(0);
+    d2.setHours(0,0,0,0);
     return d1.getTime() === d2.getTime();
   }
   formatDate(date) {
@@ -24,6 +22,17 @@ class CommonService {
   handleErrorResponse(error) {
     // TODO Implement proper handling via toaster type notification.
     console.error(error);
+  }
+  canGetNextDay(date, query) {
+    const latestDate = new Date(date);
+    latestDate.setHours(0,0,0,0);
+    latestDate.setDays(latestDate.getDate() + 1);
+    const lastDay = {
+      month: query.month ? query.month + 1 : 0,
+      year: query.month ? query.year : query.year + 1,
+    };
+    const lastDateOfQuery = new Date(lastDay.year, lastDay.month, 0);
+    return latestDate.getTime() < lastDateOfQuery.getTime();
   }
 }
 
