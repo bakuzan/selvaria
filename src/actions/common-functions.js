8cd085d76a61f978d717f57849c6e9f80e041470
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-11 22:02:56 +0000

    working on row detail

--- src/actions/common-functions.js
+++ src/actions/common-functions.js
@@ -26,7 +26,7 @@ class CommonService {
   canGetNextDay(date, query) {
     const latestDate = new Date(date);
     latestDate.setHours(0,0,0,0);
-    latestDate.setDays(latestDate.getDate() + 1);
+    latestDate.setDate(latestDate.getDate() + 1);
     const lastDay = {
       month: query.month ? query.month + 1 : 0,
       year: query.month ? query.year : query.year + 1,
--- src/components/day-row/day-row.js
+++ src/components/day-row/day-row.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import TimeBlock from '../../components/time-block/time-block';
+import './day-row.css';
 
 class DayRow extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isExpanded: false,
+      detail: []
+    };
+
+  }
+  generateDetail(times, detail) {
+    
+  }
+  toggleRowDetail() {
+    this.setState(prevState => {
+      let detail = prevState.detail;
+      if (!prevState.isExpanded && detail.length === 0) {
+        detail = this.generateDetail(this.props.item.times, detail);
+      }
+      return { isExpanded: !prevState.isExpanded, detail: detail };
+    });
+  }
   getTimes(array) {
     return array.map((item) => {
       return <TimeBlock key={item.id} item={item}
@@ -9,12 +30,21 @@ class DayRow extends Component {
     });
   }
   render() {
+    const classes = `day-row${this.state.isExpanded ? ' is-expanded ' : ' ' }center-contents`
     return (
-      <li className="day-row center-contents">
-          <div className="date">{ this.props.item.dateString }</div>
+      <li className={classes}>
+          <div className="date">
+            <button type="button" className="button ripple" onClick={() => this.toggleRowDetail()}>{ this.props.item.dateString }</button>
+          </div>
           <div className="times">
             { this.getTimes(this.props.item.times) }
           </div>
+          {
+            this.state.isExpanded &&
+            <div className="day-row-detail">
+              OUTPUT LIST OF COUNTS BY TIME (I.E 1H, 3.5H ETC.) OF EACH CATEGORY FOR THAT DAY
+            </div>
+          }
       </li>
     );
   }
