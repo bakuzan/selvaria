0a70be214b441bbed81a5fc7eec4d726c76a6797
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-28 10:09:25 +0000

    Create tab-container.js
    Tab container skeleton.

---
+++ src/components/tab-container/tab-container.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class TabContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tabs: [],
+      active: null
+    };
+  }
+  handleTabRegister() {
+    
+  }
+  handleTabChange() {
+  
+  }
+  render() {
+    return (
+      <div className="tab-container">
+      
+      </div>
+    );
+  }
+}
+
+export default TabContainer;
