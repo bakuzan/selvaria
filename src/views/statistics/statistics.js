b5da958d828933cf1d1281a7191f0c4b976037fe
bakuzan <imannoyed4days@hotmail.co.uk>
2017-02-28 14:55:31 +0000

    Update statistics.js
    Extracted category lists.
    Refactored render method.

--- src/views/statistics/statistics.js
+++ src/views/statistics/statistics.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import LoadingSpinner from '../../components/loading-spinner/loading-spinner';
 import ActionBar from '../../components/action-bar/action-bar';
-import CategoryItem from '../../components/category-item/category-item';
+import CategoryList from '../../components/category-list/category-list';
 import CommonService from '../../actions/common-service.js';
 import StatisticsQuery from '../../actions/query/statistics-query';
 import DataService from '../../actions/data-service.js';
@@ -41,9 +41,20 @@ class Statistics extends Component {
     console.log('update: ', name, value, updatedQuery);
     this.setState({ query: updatedQuery })
   }
+  renderDayOfWeekList(dayCounts) {
+    return dayCounts.map(item => {
+      return (
+        <li key={item.dayName}>
+          <span>{item.dayName}</span>
+          <CategoryList items={item.counts} />
+        </li>
+      );
+    })
+  }
   render() {
     const queryString = CommonService.constructQueryText(this.state.query);
     const showStatisticsDisplay = this.state.statistics.queryCounts.length > 0 || this.state.statistics.dayOfWeekCounts.length > 0;
+    const dayOfWeekCounts = this.renderDayOfWeekList(this.state.statistics.dayOfWeekCounts);
     console.log('statistics render : ', this.state);
 
     return (
@@ -76,32 +87,9 @@ class Statistics extends Component {
           {
             showStatisticsDisplay &&
             <div className="category-detail">
-              <ul className="category-list">
-              {
-                this.state.statistics.queryCounts.map((item, index) => {
-                  return (
-                    <CategoryItem key={index} item={item} />
-                  );
-                })
-              }
-              </ul>
-              <ul className="category-list">
-              {
-                this.state.statistics.dayOfWeekCounts.map((item, index) => {
-                  return (
-                    <li key={index}>
-                      <span>{item.dayName}</span>
-                      <ul>
-                      {
-                        item.counts.map((item, index) => {
-                          return (<CategoryItem key={index} item={item} />);
-                        })
-                      }
-                      </ul>
-                    </li>
-                  );
-                })
-              }
+              <CategoryList items={this.state.statistics.queryCounts} />
+              <ul className="day-of-week-list">
+              { dayOfWeekCounts }
               </ul>
             </div>
           }
