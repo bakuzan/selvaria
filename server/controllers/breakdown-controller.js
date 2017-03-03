c7f15d3b86e3d48f9b906c50fd52ac801f2d6cd4
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-03 16:19:39 +0000

    Update breakdown-controller.js
    category is an object in constants

--- server/controllers/breakdown-controller.js
+++ server/controllers/breakdown-controller.js
@@ -6,7 +6,7 @@ module.exports = () => {
     performFunctionsOnCounts: (categoryCounts, total) => {
       const minimumsAndMaximums = [];
       for (let i = 0, length = Constants.categories.length; i < length; i++) {
-        const category = Constants.categories[i];
+        const category = Constants.categories[i].name;
         const items = categoryCounts.filter(x => x.category === category);
         if (items.length === 0) continue;
 
