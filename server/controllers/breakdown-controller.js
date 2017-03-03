634b92191b751e9c06e1c889bd51ab611091fc16
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-03 16:18:00 +0000

    Update breakdown-controller.js
    Added mathematical functions.

--- server/controllers/breakdown-controller.js
+++ server/controllers/breakdown-controller.js
@@ -1,15 +1,36 @@
 const Constants = require('../constants');
+const CommonController = require('./common-controller.js')();
 
 module.exports = () => {
   const breakdown = {
-    calculateMinimumCount: () => {
-      return 'placeholder for min';
+    performFunctionsOnCounts: (categoryCounts, total) => {
+      const minimumsAndMaximums = [];
+      for (let i = 0, length = Constants.categories.length; i < length; i++) {
+        const category = Constants.categories[i];
+        const items = categoryCounts.filter(x => x.category === category);
+        if (items.length === 0) continue;
+
+        const min = breakdown.getObjectWithMinimum(items);
+        const max = breakdown.getObjectWithMaximum(items);
+        const average = breakdown.calculateAverageOccurance(items, total);
+        minimumsAndMaximums.push({ min, max, average, category });
+      }
+      return minimumsAndMaximums;
     },
-    calculateMaximumCount: () => {
-      return 'placeholder for max';
+    getObjectWithMinimum: (array) => {
+      return CommonController.reduceArrayWith(array, (prev, curr) => prev < curr);
     },
-    calculateAverageCounts: () => {
-      return 'placeholder for average';
+    getObjectWithMaximum: (array) => {
+      return CommonController.reduceArrayWith(array, (prev, curr) => prev > curr);
+    },
+    calculateAverageOccurance(array, total) {
+      let i = array.length;
+      let sum = 0;
+      while (i--) { 
+        sum = sum + array[i].count;
+      }
+      total = total || array.length;
+      return (sum / total).toFixed(2);
     }
   };
 
