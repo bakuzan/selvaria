028270b5c698bb748ecc38e089380dc55c05fb5f
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-13 15:28:53 +0000

    Update breakdown-controller.js
    Fix for average calculation. Should be based on all days, not just the days the category appeared on.

--- server/controllers/breakdown-controller.js
+++ server/controllers/breakdown-controller.js
@@ -5,6 +5,7 @@ module.exports = () => {
   const breakdown = {
     performFunctionsOnCounts: (categoryCounts) => {
       const minimumsAndMaximums = [];
+      const total = CommonController.findDistinct(categoryCounts, 'date').length;
       for (let i = 0, length = Constants.categories.length; i < length; i++) {
         const category = Constants.categories[i].name;
         const items = categoryCounts.filter(x => x.category === category);
@@ -12,7 +13,7 @@ module.exports = () => {
 
         const min = breakdown.getObjectWithMinimum(items);
         const max = breakdown.getObjectWithMaximum(items);
-        const average = breakdown.calculateAverageOccurance(items, 'date');
+        const average = breakdown.calculateAverageOccurance(items, total);
         minimumsAndMaximums.push({ min, max, average, category });
       }
       // console.log('min-max-avg : ', minimumsAndMaximums);
@@ -24,13 +25,12 @@ module.exports = () => {
     getObjectWithMaximum: (array) => {
       return CommonController.reduceArrayWith(array, (prev, curr) => prev > curr);
     },
-    calculateAverageOccurance: (array, property) => {
+    calculateAverageOccurance: (array, total) => {
       let i = array.length;
       let sum = 0;
       while (i--) {
         sum = sum + array[i].count;
       }
-      const total = property ? CommonController.findDistinct(array, property).length : array.length;
       return (sum / total).toFixed(2);
     }
   };
