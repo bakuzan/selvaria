f066c5b69fb007239365ec704f577f64f3434f03
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-13 06:11:22 +0100

    added express route for running in "production" mode

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use('/selvaria/static', express.static(path.resolve(__dirname, '..', 'build/static')));
 
 //Body parsing for POST-ing
 app.use(bodyParser.urlencoded({
@@ -23,5 +23,12 @@ app.use(bodyParser.json());
 
 // Routes
 app.use(require('./routes'));
+
+// Always return the main index.html, so react-router render the route in the client
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
